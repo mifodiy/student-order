7acb97a7087baf1e21aaf025986e2ee0c16da349
mifodiy911 <mifodiy911@gmail.com>
2020-05-11 20:16:04 +0300

    delete TransportException modified config.properties(added cr_url)

--- student-order/src/test/java/edu/javacourse/studentorder/validator/register/FakeCityRegisterChecker.java
+++ student-order/src/test/java/edu/javacourse/studentorder/validator/register/FakeCityRegisterChecker.java
@@ -5,7 +5,6 @@ import edu.javacourse.studentorder.domain.Child;
 import edu.javacourse.studentorder.domain.register.CityRegisterResponse;
 import edu.javacourse.studentorder.domain.Person;
 import edu.javacourse.studentorder.exception.CityRegisterException;
-import edu.javacourse.studentorder.exception.TransportException;
 
 public class FakeCityRegisterChecker implements CityRegisterChecker
 {
@@ -19,7 +18,7 @@ public class FakeCityRegisterChecker implements CityRegisterChecker
     public static final String ERROR_T_2 = "2003";
 
 
-    public CityRegisterResponse checkPerson(Person person) throws CityRegisterException, TransportException {
+    public CityRegisterResponse checkPerson(Person person) throws CityRegisterException{
         CityRegisterResponse res = new CityRegisterResponse();
         if(person instanceof Adult){
             Adult t = (Adult) person;
@@ -36,10 +35,7 @@ public class FakeCityRegisterChecker implements CityRegisterChecker
                 CityRegisterException ex = new CityRegisterException("1","GRN ERROR" + ps);
                 throw ex;
             }
-            if(ps.equals(ERROR_T_1) || ps.equals(ERROR_T_2)){
-                TransportException ex = new TransportException("Transport ERROR" + ps);
-                throw ex;
-            }
+
 
         }
         if (person instanceof Child){
